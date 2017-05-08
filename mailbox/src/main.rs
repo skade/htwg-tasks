08a647a802429d826d15d68ff34474c3207e9524
Florian Gilcher <florian.gilcher@asquera.de>
2017-05-09 00:34:37 +0200

    Full blown error handling

--- mailbox/src/main.rs
+++ mailbox/src/main.rs
@@ -5,46 +5,61 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::collections::VecDeque;
 
+#[derive(Debug)]
+enum ServerError {
+    ParseError(redisish::Error),
+    IoError(std::io::Error)
+}
+
+impl From<redisish::Error> for ServerError {
+    fn from(e: redisish::Error) -> ServerError {
+        ServerError::ParseError(e)
+    }
+}
+
+impl From<std::io::Error> for ServerError {
+    fn from(e: std::io::Error) -> ServerError {
+        ServerError::IoError(e)
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     let mut storage = VecDeque::new();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut s) => {
-                handle(&mut s, &mut storage)
-            }
-            Err(e) => {
-                println!("Connection error: {}", e);
-            }
+        let res = stream.map_err(|e| e.into() )
+                  .and_then(|mut s| {
+                      handle(&mut s, &mut storage)
+                  });
+        
+        if let Err(e) = res {
+            println!("Error occured: {:?}", e);
         }
     }
 }
 
-fn handle(stream: &mut TcpStream, storage: &mut VecDeque<String>) {
-    let command = read_command(stream);
+fn handle(stream: &mut TcpStream, storage: &mut VecDeque<String>) -> Result<(), ServerError> {
+    let command = read_command(stream)?;
     match command {
-        Ok(redisish::Command::Publish(message)) => {
+        redisish::Command::Publish(message) => {
             storage.push_back(message);
+            Ok(())
         }
-        Ok(redisish::Command::Retrieve) => {
+        redisish::Command::Retrieve => {
             let data = storage.pop_front();
             match data {
-                Some(message) => write!(stream, "{}", message),
-                None => write!(stream, "No message in inbox!\n")
-            }.expect("Write failed!");
-        }
-        Err(e) => {
-            write!(stream, "Error: {:?}\n", e).expect("Write failed!");
+                Some(message) => write!(stream, "{}", message).map_err( |e| e.into() ),
+                None => write!(stream, "No message in inbox!\n").map_err( |e| e.into() )
+            }
         }
     }
 }
 
-fn read_command(stream: &mut TcpStream) -> Result<redisish::Command, redisish::Error> {
+fn read_command(stream: &mut TcpStream) -> Result<redisish::Command, ServerError> {
     let mut read_buffer = String::new();
     let mut buffered_stream = BufReader::new(stream);
-    let res = buffered_stream.read_line(&mut read_buffer);
-    res.ok().expect("An error occured while reading!");
-    redisish::parse(&read_buffer)
+    buffered_stream.read_line(&mut read_buffer)?;
+    redisish::parse(&read_buffer).map_err( |e| e.into() )
 }
