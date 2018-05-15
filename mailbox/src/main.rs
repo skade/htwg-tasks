85071390d88e87b718544f4130bb4e2b31cb0d52
Florian Gilcher <florian.gilcher@asquera.de>
2018-05-15 14:05:21 +0200

    Mailbox without threads

--- mailbox/src/main.rs
+++ mailbox/src/main.rs
@@ -4,42 +4,11 @@ use std::net::{TcpListener,TcpStream};
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::collections::VecDeque;
-use std::sync::{Mutex,Arc};
-
-struct SyncedMailbox<T> {
-    inner: Mutex<VecDeque<T>>
-}
-
-impl<T> SyncedMailbox<T> {
-    fn new() -> Self {
-        SyncedMailbox { inner: Mutex::new(VecDeque::new()) }
-    }
-}
-
-trait Storage<T> {
-    fn add_message(&self, message: T) -> Result<(), ServerError>;
-    fn retrieve_message(&self) -> Result<Option<T>, ServerError>;
-}
-
-impl<T> Storage<T> for SyncedMailbox<T> {
-    fn add_message(&self, message: T) -> Result<(), ServerError> {
-        self.inner.lock().map( |mut guard| {
-            guard.push_back(message)
-        }).map_err( |err| err.into() )
-    }
-
-    fn retrieve_message(&self) -> Result<Option<T>, ServerError> {
-        self.inner.lock().map( |mut guard| {
-            guard.pop_front()
-        }).map_err( |err| err.into() )
-    }
-}
 
 #[derive(Debug)]
 enum ServerError {
     ParseError(redisish::Error),
     IoError(std::io::Error),
-    LockError
 }
 
 impl From<redisish::Error> for ServerError {
@@ -54,41 +23,33 @@ impl From<std::io::Error> for ServerError {
     }
 }
 
-impl<T> From<std::sync::PoisonError<T>> for ServerError {
-    fn from(_: std::sync::PoisonError<T>) -> ServerError {
-        ServerError::LockError
-    }
-}
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    let storage = Arc::new(SyncedMailbox::new());
+    let mut storage = VecDeque::new();
 
     for stream in listener.incoming() {
-        let storage_handle = storage.clone();
 
-        std::thread::spawn(move || {
-            let res = stream.map_err(|e| e.into() )
-                    .and_then(|mut s| {
-                        handle(&mut s, storage_handle.as_ref())
-                    });
-        
-            if let Err(e) = res {
-                println!("Error occured: {:?}", e);
-            }
-        });
+        let res = stream.map_err(|e| e.into() )
+                  .and_then(|mut s| {
+                     handle(&mut s, &mut storage)
+                  });
+
+        if let Err(e) = res {
+            println!("Error occured: {:?}", e);
+        }
     }
 }
 
-fn handle<S: Storage<String>>(stream: &mut TcpStream, storage: &S) -> Result<(), ServerError> {
+fn handle(stream: &mut TcpStream, storage: &mut VecDeque<String>) -> Result<(), ServerError> {
     let command = read_command(stream)?;
     match command {
         redisish::Command::Publish(message) => {
-            storage.add_message(message).map_err(|e| e.into())
+            storage.push_back(message);
+            Ok(())
         }
         redisish::Command::Retrieve => {
-            let data = storage.retrieve_message()?;
+            let data = storage.pop_front();
             match data {
                 Some(message) => write!(stream, "{}", message).map_err( |e| e.into() ),
                 None => write!(stream, "No message in inbox!\n").map_err( |e| e.into() )
